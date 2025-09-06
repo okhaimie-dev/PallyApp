c27c9cdaea4720b76a866514be7e04bd17f9b5b4
supreme2580 <victoromorogbe69@gmail.com>
2025-09-06 04:21:51 +0100

    Refactor Podfile and project configuration: Remove deprecated xcfilelist settings and unnecessary test framework references from Podfile and project.pbxproj, enhancing project clarity and maintainability.

---
+++ ios/Flutter/flutter_export_environment 3.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/victoromorogbe/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/victoromorogbe/Documents/pally"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/victoromorogbe/Documents/pally/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_CLI_BUILD_MODE=debug"
+export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzYuMC0wLjUucHJl,RkxVVFRFUl9DSEFOTkVMPWJldGE=,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NmM3OTQ4NDIxMA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTZmNjRjMjY0Nw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4wIChidWlsZCAzLjEwLjAtNzUuMS5iZXRhKQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/victoromorogbe/Documents/pally/.dart_tool/package_config.json"
